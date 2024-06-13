56e542bff77584a8ef76db6f2a6ec5f5a47f33c1
Bhaskar872kumar <157004909+Bhaskar872kumar@users.noreply.github.com>
2024-06-13 21:53:29 +0530

    Create MyToken.sol

---
+++ MyToken.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+contract Token {
+    string public name = "Token";
+    string public abbrv = "TK";
+    uint public totalSupply;
+
+    // Mapping from addresses to balances
+    mapping(address => uint) public balances;
+
+    // Mint function to Create New tokens
+    function mint(address _to, uint _value) public {
+        totalSupply += _value;
+        balances[_to] += _value;
+    }
+
+    // Burn function to Destroy tokens
+    function burn(address _from, uint _value) public {
+    if (balances[_from] >= _value) {
+        totalSupply -= _value;
+        balances[_from] -= _value;
+    } else {
+        revert("Insufficient balance to burn");
+    }
+}
+
+}
